bacc28c16fc0489934171b8ad365e06686146a8d
David Zukowski <zukoweb@gmail.com>
2015-11-23 10:36:59 -0500

    feat(vscode): add javascript snippets and keymap overrides

---
+++ vscode/keymap.js
@@ -0,0 +1,18 @@
+// Place your key bindings in this file to overwrite the defaults
+[
+{ "key": "cmd+shift+up",          "command": "editor.action.insertCursorAbove",
+                                     "when": "editorTextFocus" },
+{ "key": "cmd+shift+down",        "command": "editor.action.insertCursorBelow",
+                                     "when": "editorTextFocus" },
+{ "key": "cmd+shift+d",           "command": "editor.action.copyLinesDownAction",
+                                     "when": "editorTextFocus" },
+//  Windows keyboard mappings for OSX
+{ "key": "cmd+ctrl+up",           "command": "editor.action.moveLinesUpAction",
+                                     "when": "editorTextFocus" },
+{ "key": "cmd+ctrl+down",         "command": "editor.action.moveLinesDownAction",
+                                     "when": "editorTextFocus" },
+{ "key": "ctrl+left",             "command": "cursorWordLeft",
+                                     "when": "editorTextFocus" },
+{ "key": "ctrl+right",             "command": "cursorWordRight",
+                                     "when": "editorTextFocus" }
+]
