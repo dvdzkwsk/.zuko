a10319ef56b322fc574fad88b4fc6985d78e73ab
David Zukowski <zukoweb@gmail.com>
2017-11-23 10:29:41 -0600

    feat(make): prototype dependency installer

--- make.sh
+++ make.sh
@@ -15,10 +15,13 @@ function column {
   str+="$1$(printf " %0.s" {1..20})"
   echo "${str:0:20}"
 }
+function command_exists {
+  type "$1" &> /dev/null
+}
 
-echo ""
+echo
 log "Symlinking ${PWD}/dotfiles/* to ${HOME}"
-echo ""
+echo
 for file in dotfiles/*; do
   file=${file##*/}
   dest="${HOME}/.${file}"
@@ -36,3 +39,43 @@ for file in dotfiles/*; do
   fi
   echo "${status}  $(column $file) --> $(column "~/.${file}") ${info}"
 done
+
+function install_dependencies {
+  log "Installing missing dependencies: $1"
+  brew install $1
+}
+
+echo
+log "Checking dependencies..."
+echo
+declare -a deps=(
+  "jq"
+  "nvm"
+  "postgres"
+  "rg"
+  "tree"
+  "watchman"
+  "yarn"
+)
+declare -a missing_deps=()
+for dep in "${deps[@]}"
+do
+  if command_exists $dep ; then
+    echo "${OK}  $(column $dep) (Already installed)"
+  else
+    missing_deps+=("$dep")
+  fi
+done
+
+echo
+if [ ${#missing_deps[@]} -eq 0 ]; then
+  echo "No missing dependencies!"
+else
+  echo "Missing dependencies: ${cyan}${missing_deps}${reset}"
+  read -p "Install these dependencies (Y/n)? " -n 1 -r
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    echo
+    echo
+    install_dependencies ${missing_deps}
+  fi
+fi
