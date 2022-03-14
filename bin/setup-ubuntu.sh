8f024afaabf01cace03ff2e33f8618185eb368f1
David Zukowski <david@zuko.me>
2022-03-13 20:29:29 -0500

    sync

--- bin/setup-ubuntu.sh
+++ bin/setup-ubuntu.sh
@@ -2,21 +2,46 @@
 # Provisions a fresh Ubuntu installation
 
 # Ensure package manager is up to date
-sudo apt-get update && apt-get -y upgrade
+sudo apt-get update
+sudo apt-get upgrade
+
+# Change shell to zsh
+chsh -s /usr/bin/zsh
 
 # The Basics
 sudo apt-get -y install build-essential git zsh tmux curl
 
-# Install Neovim
-sudo apt-get -y install python-neovim python3-neovim neovim
+# Install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# Install Node
+# Core utilities
+brew install fasd bat hub neovim
+
+# Install go
+brew install go
+
+# Install node
 curl -L https://git.io/n-install | bash
 
-# Install Yarn
+# Install pnpm
+sudo apt-get install build-essential
+sudo snap install curl
+curl -L https://git.io/n-install | bash
+curl -fsSL https://get.pnpm.io/install.sh | sh
+
+# Install yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update && sudo apt install yarn
 
-# Install Nginx
+# Install nginx
 sudo apt-get -y install nginx
+
+# Notes
+#
+# Map caps lock to escape
+# $ gnome-tweaks
+# Keyboard and Mouse → Additional Layout Options -> Caps Lock Behavior -> Make Caps Lock an additional Esc
+#
+# Change keyboard repeat rate
+# Settings → Universal Access → Typing → Repeat Keys
